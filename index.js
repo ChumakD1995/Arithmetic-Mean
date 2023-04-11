680ab9d82e474b13c8294a3031a9ac633c2354a6
chumak8088@gmail.com <k.smulska@pro-capital.local>
2023-04-11 23:29:00 +0300

    arith mean

---
+++ index.js
@@ -0,0 +1,9 @@
+let num1 = prompt("");
+let num2 = prompt("");
+let num3 = prompt(""); 
+num1 = Number(num1);
+num2 = Number(num2);
+num3 = Number(num3);
+const avg = (num1 + num2 + num3) / 3; 
+console.log(avg);
+alert (`${avg}`); 
